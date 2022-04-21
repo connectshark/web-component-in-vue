915b88e6407e114e768999713720b20c50ff031f
Chambers <46649021+connectshark@users.noreply.github.com>
2022-04-21 17:37:22 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 class="title">{{title}}</h1>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+  setup () {
+    return {
+      title: 'Hello'
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+#app {
+  font-family: 'Roboto Mono', 'Noto Sans TC', sans-serif;
+  text-align: center;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import './assets/css/reset.css'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+  .use(router)
+  .mount('#app')
